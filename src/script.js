98c3c242015345a3d01725d40dca86fc8ffa4772
朝猫 <155109251+AsaNekoo@users.noreply.github.com>
2024-12-09 18:43:56 +0000

    МІЛУ ЛІНИВА ЖОПКА 	new file:   .devcontainer/devcontainer.json 	modified:   index.html 	modified:   src/script.js 	modified:   src/styles.css

--- src/script.js
+++ src/script.js
@@ -1,5 +1,25 @@
-// Function to copy to clipboard and show feedback
-function copyToClipboard(text, feedbackElement, buttonElement) {
+
+(function() {
+    // Add event listener
+    document.addEventListener("mousemove", parallax);
+    const elem = document.querySelector("#bg");
+    // Magic happens here
+    function parallax(e) {
+        let _w = window.innerWidth/2;
+        let _h = window.innerHeight/2;
+        let _mouseX = e.clientX;
+        let _mouseY = e.clientY;
+        let _depth1 = `${50 - (_mouseX - _w) * 0.01}% ${50 - (_mouseY - _h) * 0.01}%`;
+        let _depth2 = `${50 - (_mouseX - _w) * 0.01}% ${50 - (_mouseY - _h) * 0.01}%`;
+        let _depth3 = `${50 - (_mouseX - _w) * 0.01}% ${50 - (_mouseY - _h) * 0.01}%`;
+        let x = `${_depth3}, ${_depth2}, ${_depth1}`;
+        console.log(x);
+        elem.style.backgroundPosition = x;
+    }
+
+})();
+
+function copyToClipboard(text, feedbackElement, buttonElement) { // Function to copy to clipboard and show feedback
     navigator.clipboard.writeText(text).then(function() {
         // Hide the button text and icon while the feedback is visible
         if (buttonElement) {
